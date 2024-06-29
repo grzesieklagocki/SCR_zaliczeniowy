4b73304ba66683d957d25663afc0df377655caa4
Grzegorz Łagocki <grzesieklagocki@gmail.com>
2024-06-29 20:02:35 +0200

    add array with 0-9 patterns, tested on single display

--- main.c
+++ main.c
@@ -4,17 +4,37 @@
 
 int main(void)
 {
+	// inicjalizacja
+
+	// PB[0-5] jako wyjscia - do wyboru wyswietlacza
 	DDRB |= (1 << PB0) | (1 << PB1) | (1 << PB2) | (1 << PB3) | (1 << PB4) | (1 << PB5);
+	
+	// PC[0-6] jako wyjscia - sterowanie wyswietlaczem 7-segmentowym
 	DDRC |= (1 << PC0) | (1 << PC1) | (1 << PC2) | (1 << PC3) | (1 << PC4) | (1 << PC5) | (1 << PC6); 
 	
-	PORTC = 0xFF;
+	const uint8_t segments[10] = 
+	{ //	a			 b			  c			   d			e			 f			 g
+		(1 << PC0) | (1 << PC1) | (1 << PC2) | (1 << PC3) | (1 << PC4) | (1 << PC5)             , // 0
+					 (1 << PC1) | (1 << PC2)                                                    , // 1
+		(1 << PC0) | (1 << PC1)				 | (1 << PC3) | (1 << PC4) | 			  (1 << PC6), // 2
+		(1 << PC0) | (1 << PC1) | (1 << PC2) | (1 << PC3) |							  (1 << PC6), // 3
+					 (1 << PC1) | (1 << PC2) 			  | (1 << PC5) | (1 << PC6)				, // 4
+		(1 << PC0) |			  (1 << PC2) | (1 << PC3) |			     (1 << PC5)	| (1 << PC6), // 5
+		(1 << PC0) | 			  (1 << PC2) | (1 << PC3) | (1 << PC4) | (1 << PC5) | (1 << PC6), // 6
+		(1 << PC0) | (1 << PC1) | (1 << PC2) 													, // 7
+		(1 << PC0) | (1 << PC1) | (1 << PC2) | (1 << PC3) | (1 << PC4) | (1 << PC5) | (1 << PC6), // 8
+		(1 << PC0) | (1 << PC1) | (1 << PC2) | (1 << PC3) | 			 (1 << PC5) | (1 << PC6)  // 9
+	};
 
 	while(1)
 	{
-		for(int i = 0; i < 6; i++)
+		// aktywacja 1. wyswietlacza
+		PORTB = 0;
+		
+		for(int i = 0; i < 10; i++)
 		{
-				PORTB = (1 << i);
-				_delay_ms(5);
+			PORTC = segments[i]; // wysterowanie wyswietlacza
+			_delay_ms(1000);
 		}
 	}
 	
